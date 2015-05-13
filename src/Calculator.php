3459fd884a4e569a5e84d23c847a7bcf7453be5a
Miller P. Magalhães <miller@agencia110.com.br>
2015-05-13 18:03:11 -0300

    Adicionado a classe "Calculator" para ser utilizada no teste unitario.

---
+++ src/Calculator.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Squid\LetMeIn\Calculator;
+
+class Calculator {
+
+    public function sum($x, $y) {
+        return $x + $y;
+    }
+
+    public function subtract($x, $y) {
+        return $x - $y;
+    }
+
+    public function multiply($x, $y) {
+        return $x * $y;
+    }
+
+    public function divide($x, $y) {
+        return $x / $y;
+    }
+
+}
